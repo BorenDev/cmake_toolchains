1bc91b65a17e4153329170206ec7471d40f69cd8
borendev <157404740+BorenDev@users.noreply.github.com>
2025-01-03 21:28:05 -0700

    update paths and support boost libs

--- mingw_toolchain.cmake
+++ mingw_toolchain.cmake
@@ -7,13 +7,17 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(TOOLCHAIN_PREFIX "x86_64-w64-mingw32")
 
 # Allow user to designate 32 bit prefix
-if(MINGW_PLATFORM_32)
+if (${MINGW_PLATFORM_32})
   set(TOOLCHAIN_PREFIX "i686-w64-mingw32")
 endif()
 
+if (${USE_BOOST})
+    set(BOOST_ROOT "/usr/lib/${TOOLCHAIN_PREFIX}")
+endif()
+
 # Cross compilers to use for C, C++
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_C_COMPILER /usr/bin/${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/${TOOLCHAIN_PREFIX}-g++)
 
 # Add path to find cross compiler toolchain binaries This path is expected for a
 # linux build container with the mingw toolchains installed
